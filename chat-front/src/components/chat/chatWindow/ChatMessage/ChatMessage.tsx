231fa1c0160ab5c21fc80fb7c33cbd9f16e85f8f
Xipapoint <jjoster301@gmail.com>
2024-08-29 23:53:18 +0300

    added reply logic

--- chat-front/src/components/chat/chatWindow/ChatMessage/ChatMessage.tsx
+++ chat-front/src/components/chat/chatWindow/ChatMessage/ChatMessage.tsx
@@ -5,24 +5,28 @@ import { IMessage } from '../../../../models/chat/message.interface';
 interface IChatMessageProps {
   message: IMessage
   text: string;
-  isMine: boolean; 
+  isMine: boolean;
   replyMessageText: string | undefined;
   replyFirstName: string | undefined
   replyLastName: string | undefined
   isMessageReplied: boolean,
   setReplayToMessage: (message: IMessage | null) => void
   replyingMessage?: IMessage | null
+  messageRef: React.RefObject<HTMLDivElement>
+  scrollToMessage: (replyToMessage: IMessage) => void
 }
 
 const ChatMessage: React.FC<IChatMessageProps> = ({
   setReplayToMessage,
-  message, 
-  isMine, 
+  message,
+  isMine,
   replyMessageText,
   replyFirstName,
   replyLastName,
   isMessageReplied,
-  replyingMessage
+  replyingMessage,
+  messageRef,
+  scrollToMessage
 }) => {
   const [startX, setStartX] = useState<number | null>(null);
   const [isSwiping, setIsSwiping] = useState<boolean>(false);
@@ -34,25 +38,25 @@ const ChatMessage: React.FC<IChatMessageProps> = ({
     setStartX(!isMine ? divElement.left : divElement.right);
     setIsSwiping(true)
   };
-  
+
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>): void => {
     if (isSwiping && startX !== null) {
       const currentX = event.clientX;
       console.log("current ", currentX);
       console.log("start ", startX);
-      
-      
+
+
       const diff = isMine ? currentX + startX : currentX - startX;
       setPositionX(currentX)
       console.log("trying to move ", diff);
-      
+
       if (!isMine && diff > 150) {
         console.log("dif > 100");
         setIsReplying(true)
         setReplayToMessage(message)
         setIsSwiping(false);
         setPositionX(null)
-      } else if(isMine && diff < 1870){
+      } else if (isMine && diff < 1870) {
         console.log("dif < 1870");
         setIsReplying(true)
         setReplayToMessage(message)
@@ -61,13 +65,13 @@ const ChatMessage: React.FC<IChatMessageProps> = ({
       }
     }
   };
-  
+
   const handleMouseUp = () => {
     setIsSwiping(false);
     setStartX(null);
     setPositionX(null)
   };
-  
+
   const handleMouseLeave = () => {
     setIsSwiping(false);
     setStartX(null);
@@ -77,23 +81,29 @@ const ChatMessage: React.FC<IChatMessageProps> = ({
 
   return (
     <div
-      className={`${styles.message} ${isMine ? styles.mine : styles.theirs } ` }
+      className={`${styles.message} ${isMine ? styles.mine : styles.theirs} `}
+      ref={messageRef}
     >
-      {(isMessageReplied) && (
-        <div>
-          <p>{replyFirstName + ' ' + replyLastName}</p>
-          {replyMessageText}
-        </div>
-      )}
       <div
-      className={`${styles.messageText}`}
-      onMouseDown={(event: React.MouseEvent<HTMLDivElement>) => replyingMessage?._id !== message._id ? handleMouseDown(event) : null}
-      onMouseMove={(event: React.MouseEvent<HTMLDivElement>) => replyingMessage?._id !== message._id ? handleMouseMove(event) : null}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseLeave}
-      style={{ transform: isSwiping && positionX && startX ? `translateX(${positionX - startX!}px)` : 'none' }}
-      >{message.message}</div>
-      <p className={styles.timestamp}>{new Date(message.timestamp).toLocaleTimeString().slice(0,5)}</p>
+        className={`${styles.messageText}`}
+        onMouseDown={(event: React.MouseEvent<HTMLDivElement>) => replyingMessage?._id !== message._id ? handleMouseDown(event) : null}
+        onMouseMove={(event: React.MouseEvent<HTMLDivElement>) => replyingMessage?._id !== message._id ? handleMouseMove(event) : null}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave}
+        style={{ transform: isSwiping && positionX && startX ? `translateX(${positionX - startX!}px)` : 'none' }}
+      >
+        {(isMessageReplied) && (
+          <div 
+          className={`${styles.replyMessage} ${styles.backgroundColor}`}
+          onClick={() => scrollToMessage(message)}
+          >
+            <p className={styles.author}>{replyFirstName + ' ' + replyLastName}</p>
+            <p className={styles.replyMessageText}>{replyMessageText}</p>
+          </div>
+        )}
+        {message.message}
+      </div>
+      <p className={styles.timestamp}>{new Date(message.timestamp).toLocaleTimeString().slice(0, 5)}</p>
     </div>
   );
 };
